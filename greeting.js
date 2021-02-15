d008605af706ec09772cc42afa785dc624013ee8
ywoojung <y_wj1025@yahoo.co.kr>
2021-02-15 10:43:16 +0900

    0215

--- greeting.js
+++ greeting.js
@@ -5,32 +5,35 @@ const greeting = document.querySelector('.js-greetings');
 const USER_LS = 'currentUser';
 const SHOWING_CN = 'showing';
 
+/* localStorage에 name값 저장 */
 function saveName(text) {
-
+    localStorage.setItem(USER_LS, text);
 }
 
+/* input에 입력 된 name값을 화면에 보여준다 */
 function handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault();             //이벤트 버블링 막기 위해서
     
     const currentValue = input.value;
     paintGreeting(currentValue);
 
-    localStorage.setItem()
+    saveName(currentValue);
 
 }
+
 
 function askForName() {
     form.classList.add(SHOWING_CN);
     form.addEventListener('submit', handleSubmit);
 }
 
-
-
+/* 입력 된 name값을 'Hello~ name' 으로 보여준다 */
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);      //입력 form 창 지우기
     greeting.classList.add(SHOWING_CN);     //입력된 값 보여주기
     greeting.innerText = `Hello~ ${text}`;
 }
+
 
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
@@ -44,6 +47,8 @@ function loadName() {
     }
 }
 
+
+/* 실행 */
 function init() {
     loadName();
 }
