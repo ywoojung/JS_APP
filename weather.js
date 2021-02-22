249298a8012c68b516612116ff32158d40aa4c61
ywoojung <y_wj1025@yahoo.co.kr>
2021-02-22 11:42:02 +0900

    2021-02-22

--- weather.js
+++ weather.js
@@ -11,7 +11,7 @@ function getWeather(lat, lng) {
     }).then(function(json) {
         const temperature = json.main.temp;
         const place = json.name;
-        weather.innerText = `${temperature} @ ${place}`;
+        weather.innerText = `${temperature} ℃ @ ${place}`;
     });
 }
 
